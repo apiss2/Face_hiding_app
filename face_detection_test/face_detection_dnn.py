57b69f7d48c41fae82083df21197a31abe1cc29a
apiss2 <apiss2ex@gmail.com>
2019-07-12 02:10:07 +0900

    add some functions and files

--- face_detection_test/face_detection_dnn.py
+++ face_detection_test/face_detection_dnn.py
@@ -2,7 +2,48 @@ from __future__ import division
 import cv2
 import time
 from hiding_tool import put_image, mosaic_area
-from os.path import join, dirname, abspath, normpath
+import os
+from os.path import join, dirname, abspath, normpath, exists
+import datetime
+from pathlib import Path
+
+mode_dict = {0:("put","face"), 1:("put","no_photo"), 2:("put","default_icon"),
+            3:("msc",0.1), 4:("msc",0.05), 5:("msc",0.2)}
+
+def image_converter(image_path, threshold, convert_mode):
+    if type(image_path) is str and type(convert_mode) is int: #入力の型チェック
+        #path関係
+        now_dir = Path(__file__).parent
+        image_dir = join(now_dir,"image")
+        save_dir = join(now_dir,"output_image")
+        os.makedirs(save_dir, exist_ok=True)
+
+        #顔検出
+        image = cv2.imread(image_path)
+        try:
+            bboxes = face_detection(image_path, threshold=threshold)
+        except:
+            return [1, "DNN erroer."]
+
+        #処理方法選択
+        mode, detail = mode_dict[convert_mode]
+        if mode == "put":
+            icon = join(image_dir, detail+".png")
+            out_image = put_image(image, bboxes, icon)
+        elif mode == "msc":
+            out_image = mosaic_area(image, bboxes, ratio=detail)
+        else:
+            print("error")
+            return [0, "Unexpected hiding mode."]
+
+        #画像の保存
+        now = datetime.datetime.now()
+        filename = 'img_{0:%M%S%f}.jpg'.format(now)
+        saved_image_path = join(save_dir,filename)
+        cv2.imwrite(saved_image_path, out_image)
+        return saved_image_path
+    else:
+        return [0, "Unexpected input."]
 
 def detecter(net, frame, threshold):
     frameOpencvDnn = frame.copy()
@@ -23,15 +64,21 @@ def detecter(net, frame, threshold):
             bboxes.append([x1, y1, x2, y2])
     return bboxes
 
-def face_detection(image, threshold=0.5):
-    img = cv2.imread(image) if isinstance(image, str) else image.copy()
-    configFile = normpath(join(dirname(abspath(__name__)),"models/SFD_.prototxt"))
-    modelFile = normpath(join(dirname(abspath(__name__)),"models/SFD.caffemodel"))
+def face_detection(path, threshold=0.5):
+    img = cv2.imread(path)
+    now_dir = Path(__file__).parent
+    configFile = join(now_dir, "models/SFD.prototxt")
+    modelFile = join(now_dir, "models/SFD.caffemodel")
     net = cv2.dnn.readNetFromCaffe(configFile, modelFile)
     bboxes = detecter(net, img, threshold=threshold)
     return bboxes
 
 if __name__ == "__main__" :
+    """
+    a = image_converter("C:/Users/RIKI/Documents/PythonScripts/Face_hiding_app/face_detection_test/image/sample.jpg",
+                    threshold=0.5, convert_mode=1)
+    print(a)
+    """
     t = time.time()
     image = cv2.imread("./image/sample.jpg")
     bboxes = face_detection(image, threshold=0.5)
--- face_detection_test/hiding_tool.py
+++ face_detection_test/hiding_tool.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 def draw_bbox(image, bboxes):
     for bbox in bboxes:
@@ -19,22 +20,26 @@ def paste_transparent_image(roi, t_img):
     img2gray = cv2.cvtColor(t_img,cv2.COLOR_BGR2GRAY)
     _, mask = cv2.threshold(img2gray, 10, 255, cv2.THRESH_BINARY)
     mask_inv = cv2.bitwise_not(mask)
+    #bug
+    #同じサイズの画像が来ない場合がある
+    print(roi.shape, t_img.shape)
+    print(mask_inv.shape, mask.shape)
 
     img1_bg = cv2.bitwise_and(roi,roi, mask=mask_inv)
     img2_fg = cv2.bitwise_and(t_img,t_img, mask=mask)
     return cv2.add(img1_bg,img2_fg)
 
-def put_image(image, bboxes):
+def put_image(image, bboxes, icon):
     img = image.copy()
     max_size = img.shape[0] if img.shape[0]>img.shape[1] else img.shape[1]
-    LM = cv2.imread("./image/face.png")
+    LM = cv2.imread(icon)
     LM_size = LM.shape[1]
     for bbox in bboxes:
         size = bbox[2]-bbox[0] if (bbox[2]-bbox[0])>(bbox[3]-bbox[1]) else bbox[3]-bbox[1]
         if size>max_size:
             pass
         else:
-            ratio = size/LM_size
+            ratio = size/LM_size*1.05
             resised_LM = cv2.resize(LM, None, fx=ratio, fy=ratio, interpolation=cv2.INTER_NEAREST)
             h, w, _ = resised_LM.shape
             roi = img[bbox[1]:bbox[1]+h, bbox[0]:bbox[0]+w].copy()
